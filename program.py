d4ce112cf7fd096bf69c8ab3f8ed96e5a420f080
Ginca <48185940+reactst@users.noreply.github.com>
2021-11-02 13:29:32 +0100

    final

--- program.py
+++ program.py
@@ -1,40 +1,74 @@
-from klasa import *
-b = Board()
-while True:
-    in_put = input("")
-    if in_put.find("exit") != -1:
-        print("EXITING")
-        break
-    lastPLACE = in_put.rfind("PLACE")
-    firstPOS = in_put[lastPLACE + 6:lastPLACE + 9]
-    values = firstPOS.split(',')
-    b.x = int(values[0])
-    b.y = int(values[1])
-    novistr = in_put[lastPLACE+9:]
-    if novistr.find("EAST")!=-1:
-        b.f = 'E'
-    elif novistr.find("WEST")!=-1:
-        b.f = 'S'
-    elif novistr.find("NORTH")!=-1:
-        b.f = 'S'
-    elif novistr.find("SOUTH") != -1:
-        b.f = 'S'
+def setf (f):
+    if f.find('EAST')!= -1:
+        return 'EAST'
+    elif f.find('WEST')!= -1:
+        return 'WEST'
+    elif f.find('NORTH')!= -1:
+        return 'NORTH'
+    elif f.find('SOUTH')!= -1:
+        return 'SOUTH'
     else:
-        print("nesto nevalja")
+        return 'NEVALJA'
+def move(x,y,f):
+    if f == 'NORTH' and y < 5:
+        y+=1
+    if f == 'SOUTH' and y > 0:
+        y-=1
+    if f == 'EAST' and x < 5:
+        x+=1
+    if f == 'WEST' and x > 0:
+        x-=1
+    return x,y
 
-    leftcnt = novistr.count("LEFT")
-    for i in range(leftcnt):
-        b.left()
+def left(f):
+    if f.find('NORTH')!=-1:
+        nf = 'WEST'
+    elif f.find('WEST')!=-1:
+        nf = 'SOUTH'
+    elif f.find('SOUTH')!=-1:
+        nf = 'EAST'
+    elif f.find('EAST')!=-1:
+        nf = 'NORTH'
+    return nf
 
-    rightcnt = novistr.count("RIGHT")
-    for i in range(rightcnt):
-        b.right()
+def right(f):
+    if f.find('NORTH')!=-1:
+        nf = 'EAST'
+    elif f.find('WEST')!=-1:
+        nf = 'SOUTH'
+    elif f.find('SOUTH')!=-1:
+        nf = 'WEST'
+    elif f.find('WEST')!=-1:
+        nf = 'NORTH'
+    return nf
 
-    movecnt = novi.count("MOVE")
-    for i in range(movecnt):
-        b.move()
+def report (x,y,f):
+    print (f'{x},{y},{f}')
 
-    if novistr.find("REPORT")!=-1:
-        b.report()
+while True:
+    in_put = input('')
+    if in_put.find('exit') != -1:
+        print('EXITING')
+        break
+    lastPLACE = in_put.rfind('PLACE')
+    firstPOS = in_put[lastPLACE + 6 : lastPLACE + 9]
+    values = firstPOS.split(',')
+    x = int(values[0])
+    y = int(values[1])
+    f = in_put[in_put.rfind('PLACE')+10:in_put.rfind('PLACE')+15]
+    rest = in_put[in_put.rfind('PLACE')+15:]
+    rest = rest.split(' ')
+    for i in rest:
+        f = setf(f)
+        if i == 'MOVE':
+            x,y= move(x,y,f)
+        if i == 'LEFT':
+            f = left(f)
+        if i == 'RIGHT':
+            f = right(f)
+        if i == 'REPORT':
+            report(x,y,f)
 
-print ("job done")
+
+
+
