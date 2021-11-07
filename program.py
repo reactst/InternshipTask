714ebebab296e76ea07ff6be6f88cb71441be0fa
Roko Dugum <reactst@gmail.com>
2021-11-07 01:14:53 +0100

    ufinija jos malo

--- program.py
+++ program.py
@@ -1,38 +1,32 @@
-def move(x,y,f):
+def move(x, y, f):
     if f == 'NORTH' and y < 5:
-        y+=1
+        y += 1
     if f == 'SOUTH' and y > 0:
-        y-=1
+        y -= 1
     if f == 'EAST' and x < 5:
-        x+=1
+        x += 1
     if f == 'WEST' and x > 0:
-        x-=1
-    return x,y
+        x -= 1
+    return x, y
 
-def left(f):
-    if f.find('NORTH')!=-1:
-        nf = 'WEST'
-    elif f.find('WEST')!=-1:
-        nf = 'SOUTH'
-    elif f.find('SOUTH')!=-1:
-        nf = 'EAST'
-    elif f.find('EAST')!=-1:
-        nf = 'NORTH'
-    return nf
 
-def right(f):
-    if f.find('NORTH')!=-1:
-        nf = 'EAST'
-    elif f.find('WEST')!=-1:
-        nf = 'SOUTH'
-    elif f.find('SOUTH')!=-1:
-        nf = 'WEST'
-    elif f.find('WEST')!=-1:
-        nf = 'NORTH'
-    return nf
+def report(x, y, f):
+    print(f'{x},{y},{f}')
 
-def report (x,y,f):
-    print (f'{x},{y},{f}')
+
+left = {
+    'NORTH': 'WEST',
+    'WEST': 'SOUTH',
+    'SOUTH': 'EAST',
+    'EAST': 'NORTH'
+}
+
+right = {
+    'NORTH': 'EAST',
+    'WEST': 'SOUTH',
+    'SOUTH': 'WEST',
+    'WEST': 'NORTH'
+}
 
 while True:
     in_put = input('')
@@ -46,14 +40,10 @@ while True:
             y = int(input_list[i+1].split(',')[1])
             f = input_list[i+1].split(',')[2]
         if v == 'MOVE':
-            x,y= move(x,y,f)
+            x, y = move(x, y, f)
         if v == 'LEFT':
-            f = left(f)
+            f = left[f]
         if v == 'RIGHT':
-            f = right(f)
+            f = right[f]
         if v == 'REPORT':
-            report(x,y,f)
-
-
-
-
+            report(x, y, f)
